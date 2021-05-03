a30d5acc1ac03b897f0164402effca2ea7fa17d4
soohy <tngus90!>
2021-05-03 21:19:55 +0900

    feat: 로깅 간단히 알아보기

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,30 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Slf4j // Logger 객체를 주입해 준다.
+@RestController
+public class LogTestController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+
+        //로그를 사용하지 않아도 문자열 더하기 연산이 무조건 실행되기 때문에, 이런 방식으로 사용하지 말자.
+        log.debug("String concat log=" + name);
+
+        return "ok";
+    }
+
+}
